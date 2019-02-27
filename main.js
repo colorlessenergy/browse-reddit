d5448443de10cdcdbc0cbdf95328c55f4fbe18f7
Brian Munoz <brianmunozdev@gmail.com>
2019-02-27 05:12:11 -0800

    update the subreddit dynamically

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-fetchReddit('leagueoflegends')
+let form = document.querySelector('form');
+
+form.addEventListener('submit', function (ev) {
+  ev.preventDefault();
+  let subredditBanner = document.querySelector('.subreddit-banner__subreddit');
+  let userInput = form['input-text'].value;
+
+  subredditBanner.textContent = 'r/' + userInput;
+  fetchReddit(userInput);
+
+});
